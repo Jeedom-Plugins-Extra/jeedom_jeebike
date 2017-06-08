d7da080ff1ecb78a41601676bae1f2b37493b925
Frédéric FRANCE <frederic.france@free.fr>
2017-06-08 17:29:26 +0200

    update

--- core/ajax/jeebike.ajax.php
+++ core/ajax/jeebike.ajax.php
@@ -18,12 +18,42 @@
 
 try {
     require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+    //log::add('jeebike', 'debug', 'appel ajax');
+    //log::add('jeebike', 'debug', 'get ' . print_r($_GET, true));
+    //log::add('jeebike', 'debug', 'post ' . print_r($_POST, true));
     include_file('core', 'authentification', 'php');
 
     if (!isConnect('admin')) {
         throw new Exception(__('401 - Accès non autorisé', __FILE__));
     }
 
+    if(isset($_POST['go']) || isset($_GET['go']) || isset($_POST['id_contract']) || isset($_GET['id_contract'])) {
+ 
+        $json = array();
+     
+        if(isset($_POST['go']) || isset($_GET['go'])) {
+            $jeebike = new jeebike();
+            $allcontracts = $jeebike->getAllContracts();
+            foreach ($allcontracts as $contract) {
+                $json[$contract->name][] = $contract->country_code . '-' . $contract->name . '-' . $contract->commercial_name;
+            }
+            //log::add('jeebike', 'debug', print_r($json, true));
+        } else if(isset($_POST['id_contract'])) {
+            $id = htmlentities($_POST['id_contract']); // nom du contrat
+            if ($id!='null') {
+                $jeebike = new jeebike();
+                $stations = $jeebike->getStationsForContract($id);
+                foreach ($stations as $station) {
+                    $json[$station->number][] = $station->name;
+                }
+                //log::add('jeebike', 'debug', print_r($json, true));
+            }
+        }
+     
+        // envoi du résultat au success
+        ajax::success($json);
+
+    }
 
 
     throw new Exception(__('Aucune méthode correspondante à : ', __FILE__) . init('action'));
--- core/class/jeebike.class.php
+++ core/class/jeebike.class.php
@@ -27,32 +27,68 @@ class jeebike extends eqLogic {
     /*     * ***********************Methode static*************************** */
 
     public static function cron15() {
-        foreach (eqLogic::byType('jeebike', true) as $jeebike) {
-            if ($jeebike->getIsEnable() == 1) {
-                if (!empty($jeebike->getConfiguration('apikey'))) {
-                    log::add('jeebike', 'debug', 'récuperation données');
-                } else {
-                     log::add('jeebike', 'debug', 'pas de clé api');
+        if (trim(config::byKey('apikey', 'jeebike'))!='') {
+            log::add('jeebike', 'debug', 'jeebike cron15');
+            foreach (eqLogic::byType('jeebike', true) as $jeebike) {
+                if ($jeebike->getIsEnable() == 1) {
+                    if (!empty($jeebike->getConfiguration('contract_name'))&& !empty($jeebike->getConfiguration('number'))) {
+                        log::add('jeebike', 'debug', 'récuperation données '.$jeebike->getConfiguration('contract_name').' '.$jeebike->getConfiguration('number'));
+                        $jeebike->getBikes($jeebike->getConfiguration('number'), $jeebike->getConfiguration('contract_name'), trim(config::byKey('apikey', 'jeebike')));
+                    } else {
+                        log::add('jeebike', 'debug', 'config incomplète');
+                    }
                 }
             }
+        } else {
+            log::add('jeebike', 'debug', 'pas de clé api');
         }
     }
 
+
+    public function getAllContracts() {
+        $apikey = trim(config::byKey('apikey', 'jeebike'));
+        $url ='https://api.jcdecaux.com/vls/v1/contracts?apiKey='.$apikey;
+        //log::add('jeebike', 'debug', print_r($url, true));
+        $json = file_get_contents($url);
+        $allcontracts = json_decode($json);
+        //log::add('jeebike', 'debug', print_r($allcontracts, true));
+        return $allcontracts;
+    }
+
+    public function getStationsForContract($contract) {
+        $apikey = trim(config::byKey('apikey', 'jeebike'));
+        $url ='https://api.jcdecaux.com/vls/v1/stations?contract='.$contract.'&apiKey='.$apikey;
+        //log::add('jeebike', 'debug', print_r($url, true));
+        $json = file_get_contents($url);
+        $stations = json_decode($json);
+        //log::add('jeebike', 'debug', print_r($stations, true));
+        return $stations;
+    }
+
+    public function getAllStations() {
+        $apikey = trim(config::byKey('apikey', 'jeebike'));
+        $url ='https://api.jcdecaux.com/vls/v1/stations?apiKey='.$apikey;
+        //log::add('jeebike', 'debug', print_r($url, true));
+        $json = file_get_contents($url);
+        $allstations = json_decode($json);
+        //log::add('jeebike', 'debug', print_r($allstations, true));
+        return $allstations;
+    }
 
 
 
     /*     * *********************Méthodes d'instance************************* */
 
     public function preInsert() {
-        
+
     }
 
     public function postInsert() {
-        
+
     }
 
     public function preSave() {
-        
+
     }
 
     public function postSave() {
@@ -60,13 +96,29 @@ class jeebike extends eqLogic {
     }
 
     public function preUpdate() {
-        if (empty($this->getConfiguration('apikey'))) {
-            throw new Exception(__('La clé api ne peut pas être vide',__FILE__));
+        if (empty($this->getConfiguration('contract_name'))) {
+            throw new Exception(__('La ville doit être spécifiée',__FILE__));
+        }
+        if (empty($this->getConfiguration('number'))) {
+            throw new Exception(__('La numéro de station doit être spécifié',__FILE__));
         }
     }
 
     public function postUpdate() {
         log::add('jeebike', 'debug', '** postUpdate **');
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'contract_name');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Ville', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('contract_name');
+            $jeebikeCmd->setConfiguration('data', 'contract_name');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('string');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
         $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'number');
         if (!is_object($cmdlogic)) {
             $jeebikeCmd = new jeebikeCmd();
@@ -75,9 +127,122 @@ class jeebike extends eqLogic {
             $jeebikeCmd->setLogicalId('number');
             $jeebikeCmd->setConfiguration('data', 'number');
             $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('string');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'name');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Nom', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('name');
+            $jeebikeCmd->setConfiguration('data', 'name');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('string');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'address');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Adresse', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('address');
+            $jeebikeCmd->setConfiguration('data', 'address');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('string');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'banking');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Borne de paiement', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('banking');
+            $jeebikeCmd->setConfiguration('data', 'banking');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('binary');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'bonus');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Bonus', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('bonus');
+            $jeebikeCmd->setConfiguration('data', 'bonus');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('string');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'status');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Statut', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('status');
+            $jeebikeCmd->setConfiguration('data', 'status');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('string');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'bike_stands');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Points d attache', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('bike_stands');
+            $jeebikeCmd->setConfiguration('data', 'bike_stands');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('string');
+            $jeebikeCmd->setIsHistorized(0);
+            $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'available_bike_stands');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Points d attache libres', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('available_bike_stands');
+            $jeebikeCmd->setConfiguration('data', 'available_bike_stands');
+            $jeebikeCmd->setType('info');
             $jeebikeCmd->setSubType('numeric');
             $jeebikeCmd->setIsHistorized(0);
             $jeebikeCmd->save();
+        }
+
+        $cmdlogic = jeebikeCmd::byEqLogicIdAndLogicalId($this->getId(), 'available_bikes');
+        if (!is_object($cmdlogic)) {
+            $jeebikeCmd = new jeebikeCmd();
+            $jeebikeCmd->setName(__('Vélos disponibles', __FILE__));
+            $jeebikeCmd->setEqLogic_id($this->id);
+            $jeebikeCmd->setLogicalId('available_bikes');
+            $jeebikeCmd->setConfiguration('data', 'available_bikes');
+            $jeebikeCmd->setType('info');
+            $jeebikeCmd->setSubType('numeric');
+            $jeebikeCmd->setIsHistorized(1);
+            $jeebikeCmd->save();
+        }
+
+        // recuperation données statiques
+        if ($this->getIsEnable() == 1) {
+            $this->getStationInfo($this->getConfiguration('number'), $this->getConfiguration('contract_name'), trim(config::byKey('apikey', 'jeebike')));
+        }
+        // recuperation données dynamiques
+        if ($this->getIsEnable() == 1) {
+            $this->getBikes($this->getConfiguration('number'), $this->getConfiguration('contract_name'), trim(config::byKey('apikey', 'jeebike')));
         }
     }
 
@@ -97,7 +262,82 @@ class jeebike extends eqLogic {
      */
 
     /*     * **********************Getteur Setteur*************************** */
-    public function getBikes() {
+    public function getBikes($number, $contract, $apikey) {
+        $url ='https://api.jcdecaux.com/vls/v1/stations/'.$number.'?contract='.$contract.'&apiKey='.$apikey;
+        $json = file_get_contents($url);
+        $json_ret = json_decode($json);
+        log::add('jeebike', 'debug', print_r($json_ret, true));
+        // epoch timestamp
+        $date = date("Y-m-d H:i:s", substr($json_ret->last_update, 0,10));
+
+        $cmd = $this->getCmd(null, 'status');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->status);
+        }
+
+        $cmd = $this->getCmd(null, 'bike_stands');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->bike_stands);
+        }
+ 
+        $cmd = $this->getCmd(null, 'available_bike_stands');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->available_bike_stands);
+        }
+
+        $cmd = $this->getCmd(null, 'available_bikes');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->available_bikes);
+        }
+    }
+
+    public function getStationInfo($number, $contract, $apikey) {
+        $url ='https://api.jcdecaux.com/vls/v1/stations/'.$number.'?contract='.$contract.'&apiKey='.$apikey;
+        $json = file_get_contents($url);
+        $json_ret = json_decode($json);
+        log::add('jeebike', 'debug', print_r($json_ret, true));
+        // epoch timestamp
+        $date = date("Y-m-d H:i:s", substr($json_ret->last_update, 0,10));
+
+        $cmd = $this->getCmd(null, 'number');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->number);
+        }
+
+        $cmd = $this->getCmd(null, 'contract_name');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->contract_name);
+        }
+
+        $cmd = $this->getCmd(null, 'name');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->name);
+        }
+
+        $cmd = $this->getCmd(null, 'address');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->address);
+        }
+
+        $cmd = $this->getCmd(null, 'banking');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->banking);
+        }
+
+        $cmd = $this->getCmd(null, 'bonus');
+        if (is_object($cmd)) {
+            $cmd->setCollectDate($date);
+            $cmd->event($json_ret->bonus);
+        }
     }
 }
 
